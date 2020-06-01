e633c4e2a19dbbb5e13bddb4ed9439d8eba5fb1d
Pedro Henrique <system.pedrohenrique@gmail.com>
2020-06-01 10:58:47 -0300

    start working on ci

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -1,2 +1,3 @@
+export const publicURL = 'http://agenda-slim.test'
 export const baseURL = 'http://agenda-slim.test/api'
 export const mainPortalURL = 'http://link-to-main-portal.com'
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,8 @@
 import ReactDOM from 'react-dom'
 
+import { publicURL } from './env'
+process.env.PUBLIC_URL = publicURL
+
 import { createAppComponent } from './components/App'
 import './style.css'
 
