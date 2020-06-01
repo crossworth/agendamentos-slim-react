434d50a16e67c4699894cc805c8a1cd3a4469b2f
Pedro Henrique <system.pedrohenrique@gmail.com>
2020-06-01 12:26:32 -0300

    wip

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -1,3 +1,2 @@
-export const publicURL = 'http://agenda-slim.test'
 export const baseURL = 'http://agenda-slim.test/api'
 export const mainPortalURL = 'http://link-to-main-portal.com'
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,5 @@
 import ReactDOM from 'react-dom'
-import { publicURL } from './env'
 import { createAppComponent } from './components/App'
 import './style.css'
-
-process.env.PUBLIC_URL = publicURL
 
 ReactDOM.render(createAppComponent, document.querySelector('#root'))
